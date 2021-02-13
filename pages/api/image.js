da9111e7ca8f0fb62ce294b286a5a5b5cb3abf8c
Penry Downey <penry.downey@apsleygroup.co.uk>
2021-02-13 01:33:24 +0000

    Refactor Wikipedia image endpoint - Account for non asci characters - Error handlng

--- pages/api/image.js
+++ pages/api/image.js
@@ -1,29 +1,11 @@
-const image = async (req, res) => {
+import getWikipediaThumbnail from 'lib/getWikipediaThumbnail'
 
-    const { wiki } = req.query
+export default async (req, res) => {
+    const { wiki, size = 250 } = req.query
 
-    const regex = /^.+\/wiki\/(?<title>.+)$/;
-
-    const match = wiki.match(regex)
-
-    const { title } = match.groups
-
-
-    const image = await fetch(`https://en.wikipedia.org/w/api.php?action=query&titles=${title}&redirects=1&prop=pageimages&format=json&pithumbsize=200`)
-    .then(res => res.json())
-    .then((resp) => {
-        console.log(resp)
-        return resp.query.pages
-    }).catch((error) => {
-        return false
-    })
-
+    const data = await getWikipediaThumbnail(wiki, size)
 
 
     res.statusCode = 200
-    // res.send(image)
-    // res.json(image[Object.keys(image)[0]].thumbnail.source)
-    res.redirect(301, image[Object.keys(image)[0]].thumbnail.source)
-}
-
-export default image
+    res.json(data)
+  }
